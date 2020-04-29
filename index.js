4065c2ec68cb77b991f3be4ae9900dfd11a69c4a
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-04-29 10:45:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHuToGrandma(string) {
+  if(string.toLowerCase === string){
+    return "I can't hear you!"
+  } else if(string.toUpperCase === STRING){
+    return 'YES INDEED!'
+  } else if(string === "I love you, Grandma."){
+    return 'I love you, too.'
+  }
+}
