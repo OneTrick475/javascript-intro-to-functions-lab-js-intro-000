595f822662f5833fa912efe810d2ef387f047993
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-04-29 10:48:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string.toLowerCase === string){
     return "I can't hear you!"
-  } else if(string.toUpperCase === STRING){
+  } var STRING
+  else if(string.toUpperCase === STRING){
     return 'YES INDEED!'
   } else if(string === "I love you, Grandma."){
     return 'I love you, too.'
